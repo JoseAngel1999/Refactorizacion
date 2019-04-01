1898227a999efe509b8b78273342952254d8d698
josea <josea@DESKTOP-8QCCRLS>
2019-04-01 12:35:51 +0200

    Sin IVA

---
+++ Refactorizacion1/src/es/studium/Refactorizacion/Refactorizacion.java
@@ -0,0 +1,12 @@
+package es.studium.Refactorizacion;
+
+public class Refactorizacion
+{
+
+	public double calcularPrecio(int unidades, double precioUnitario) {
+		
+		return unidades*precioUnitario;
+		
+	}
+	
+}
